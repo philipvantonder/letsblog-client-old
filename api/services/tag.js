1a6c805427ac2b437c36e367aaace37733e237a1
philip <pvantonder157@gmail.com>
2020-04-29 21:29:48 +0200

    Added created tag functionality to Tag Model

--- api/services/tag.js
+++ api/services/tag.js
@@ -4,7 +4,19 @@ module.exports = {
 
 	create: async (postDTO) => {
 
+		const checkTag = await TagModel.findOne({ name: postDTO.name });
 
+		if (checkTag) {
+			return { code: 1, message: 'Tag already exists' };
+		}
+
+		const tag = new TagModel({
+			name: postDTO.name
+		});
+
+		await tag.save();
+
+		return { code: 0, message: 'Tag have been added' };
 
 	}
 
