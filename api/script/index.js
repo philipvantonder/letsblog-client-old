4b5bc836e8558130610243f74963d965dabe866e
philip <pvantonder157@gmail.com>
2020-06-21 11:03:15 +0200

    updated scripts and updated Procfile

--- api/script/index.js
+++ api/script/index.js
@@ -1,59 +1,21 @@
-const seedRole = require('./seed/roles');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const mongoose = require('mongoose');
+const { mongo_url } = require('../config/index');
 
-seedRole.generateRoles();
+// Connect to mongo
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI || mongo_url, {
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+    useUnifiedTopology: true 
+})
+.then(() => console.log('Database is connected'))
+.catch(err => console.error(`Could not connect to DB ${err}`));
 
-// const roleModel = require('../models/roles');
-// const userRolesModel = require('../models/userRoles');
+const { generateRoles } = require('./seed');
 
-// async function generateRoles() {
-	
-	// const rolesArr = [{ name: 'Moderator' }, { name: 'Admin' }, { name: 'Test' }];
-	
-	// let added = false;
-	
-	// for (newRole of rolesArr) {
-		
-		// console.log(newRole.name);
-		
-		// const role = roleModel.findOne({ name: newRole.name });
-
-		// console.log("awe");
-		// console.log(role);
-		
-		// role.then(function (res, res1) {
-		// 	console.log(res);
-		// 	console.log(res1);
-		// });
-
-		// if (!role) {
-
-			// console.log(newRole.name);
-
-			// rolesModelObj = new RoleModel({
-			// 	name: newRole.name
-			// });
-			
-			// rolesModelObj.save();
-
-			// console.log(`New Role added: ${newRole.name}`);
-
-			// added = true;
-			
-		// }
-
-	// }
-
-	// if (!added) {
-	// 	console.log("There was no new Roles added.");
-	// }
-
-// }
-
-// generateRoles();
-
-// userRolesModelObj = new userRolesModel({
-// 	user: "5e9b2503ee770d423cdb48a6",
-// 	role: "5eda845c653add4ac0c04a70"
-// });
-
-// userRolesModelObj.save();
+router.get('/seed/roles', generateRoles);
+app.use(router);
