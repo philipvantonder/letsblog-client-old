89fbb9e41901489f40432816f9df346e44bca08e
philip <pvantonder157@gmail.com>
2020-06-22 22:33:40 +0200

    Moved api folder to own repo

--- api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const PostRoute = require('./routes/post');
-const UserRoute = require('./routes/user');
-const CategoryRoute = require('./routes/category');
-const CommentRoute = require('./routes/comment');
-
-app.use('/api/posts', PostRoute);
-app.use('/api/users', UserRoute);
-app.use('/api/category', CategoryRoute);
-app.use('/api/comment', CommentRoute);
